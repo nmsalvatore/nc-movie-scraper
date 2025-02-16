9036211ffff3eb190126cc23f823356fae5288d4
Nick Salvatore <nmsalvatore@gmail.com>
2025-02-16 09:46:29 -0800

    feat: type hints and error handling

--- src/moviescraper/json_requests.py
+++ src/moviescraper/json_requests.py
@@ -5,26 +5,35 @@ from selenium.webdriver.support.wait import WebDriverWait
 from seleniumwire import webdriver
 
 
-def get_json_requests(url):
+def get_json_requests(url: str) -> list[str]:
     """Retrieve all network requests ending in .json from a given URL"""
 
-    options = Options()
-    options.add_argument("--headless")
-    options.binary_location = "/usr/bin/chromium"
+    driver = None
 
-    service = Service("/usr/bin/chromedriver")
-    driver = webdriver.Chrome(service=service, options=options)
+    try:
+        options = Options()
+        options.add_argument("--headless")
+        options.binary_location = "/usr/bin/chromium"
+        service = Service("/usr/bin/chromedriver")
 
-    driver.get(url)
+        driver = webdriver.Chrome(service=service, options=options)
+        driver.get(url)
 
-    wait = WebDriverWait(driver, timeout=20)
-    wait.until(lambda d: d.find_element(By.TAG_NAME, "iframe"))
+        wait = WebDriverWait(driver, timeout=20)
+        wait.until(lambda d: d.find_element(By.TAG_NAME, "iframe"))
 
-    api_routes = list()
-    for request in driver.requests:
-        if request.url.endswith(".json"):
-            api_routes.append(request.url)
+        endpoints: list[str] = []
 
-    driver.quit()
+        for request in driver.requests:
+            if request.url.endswith(".json"):
+                endpoints.append(request.url)
 
-    return api_routes
+        return endpoints
+
+    except Exception as e:
+        print(f"Failed to retrieve JSON requests from {url}:", e)
+        return []
+
+    finally:
+        if driver:
+            driver.quit()
